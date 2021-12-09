ac07d6d69e249c872f6f2ef080ed13ff524d4def
Przemysław Paziewski <ppaazz56@gmail.com>
2021-12-09 19:49:37 +0100

    Webpack configuration, code split

---
+++ src/components/project-input.ts
@@ -0,0 +1,76 @@
+import { Component } from "./base";
+import { projectState } from "../state/project";
+import { validation, Validatable } from "../util/validation";
+
+// Project Input
+ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+  
+    constructor() {
+      super("project-input", "app", true, "user-input");
+      this.titleInputElement = this.element.querySelector(
+        "#title"
+      ) as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector(
+        "#description"
+      ) as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector(
+        "#people"
+      ) as HTMLInputElement;
+      this.configure();
+    }
+  
+    configure() {
+      this.element.addEventListener("submit", this.submitHandler.bind(this));
+    }
+    private gatherUserInput(): [string, string, number] | void {
+      const enteredTitle = this.titleInputElement.value;
+      const enteredDescription = this.descriptionInputElement.value;
+      const enteredPeople = this.peopleInputElement.value;
+  
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+      };
+      const descValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+      };
+      const peopleValidatable: Validatable = {
+        value: enteredPeople,
+        required: true,
+      };
+  
+      if (
+        !validation(titleValidatable) ||
+        !validation(descValidatable) ||
+        !validation(peopleValidatable)
+      ) {
+        alert("Invalid input, please try again");
+        return;
+      } else {
+        return [enteredTitle, enteredDescription, +enteredPeople];
+      }
+    }
+  
+    private clearInputs() {
+      this.titleInputElement.value = "";
+      this.descriptionInputElement.value = "";
+      this.peopleInputElement.value = "";
+    }
+  
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      const userInput = this.gatherUserInput();
+      if (Array.isArray(userInput)) {
+        const [title, desc, people] = userInput;
+        projectState.addProject(title, desc, people);
+        console.log(title, desc, people);
+        this.clearInputs();
+      }
+    }
+  
+    renderContent() {}
+  }
